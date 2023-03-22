8762dda15758441839c97530d2380df885d1d659
seba.developer <cmoralesaroca@gmail.com>
2023-03-22 01:07:20 -0300

    probando funciones de js

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-console.log("hola");
+const addElementList = document.getElementById("addElementList") ;
+const inputList = document.getElementById("inputList") ;
+const numberList = document.getElementById("numberList") ;
+const contElement = document.getElementById("contElement") ;
 
-const addElementList = document.querySelector("#addElementList") ;
-const contElement = document.querySelector("#contElement") ;
+function addElement () {
+    const createDiv = document.createElement("div") ;
+    contElement.appendChild(createDiv) ;
+    createDiv.style = "display:block; background-color:white; border:2px solid blue; border-radius:15px; margin:10px 8% 0px 0px; height:70px; padding-top:10px;" ;
+
+    const createAmaunt = document.createElement("h3") ;
+    const valueEmaunt = numberList.value ;
+    createAmaunt.innerHTML = valueEmaunt ;
+    createDiv.appendChild(createAmaunt) ;
+    createAmaunt.style = "display:inline; font-family:sans-serif; font-size:50px; font-weight:bold; color: blue; background-color: whitesmoke; margin-left:50px; padding:0px 20px 0px 20px;" ;
+
+    const createElement = document.createElement ("h3") ;
+    const valueElement = inputList.value ;
+    createElement.innerHTML = valueElement ;
+    createDiv.appendChild(createElement) ;
+    createElement.style = "display:inline; font-family:sans-serif; font-size:30px; font-weight:bold; color:black; margin-left:20px;" ;
+
+
+    const createCheck = document.createElement ("input") ;
+    createCheck.type = "checkbox" ;
+    createDiv.appendChild(createCheck) ;
+    createCheck.style = "display:inline; width:20px; height:20px; color:red; margin-left:60%;" ;
+
+    const createDelete = document.createElement ("button") ;
+    createDelete.type = "reset" ;
+    createDiv.appendChild(createDelete) ;
+    createDelete.style = "margin-left:20px; background:transparent; border:none;" ;
+
+    const createImg = document.createElement ("img") ;
+    createImg.src = "../public/delete.png" ;
+    createDelete.appendChild(createImg) ;
+    createImg.style = "width:20px; height:20px" ;
+};
+
 
 addElementList.addEventListener("click", () => {
-    console.log("hola");
-    const create = document.createElement("p");
-    create.innerHTML = "Create";
-    document.getElementById("contElement").appendChild(create);
+    console.log("Click Add Element List");
+    addElement () ;
 });
